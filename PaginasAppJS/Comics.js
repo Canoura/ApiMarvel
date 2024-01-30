b8bfb6d24e26d7001fed1997930f5fd10361543d
Canoura <sthefanyacostac@gmail.com>
2024-01-30 19:31:55 +0000

    Arreglamos el codigo para que nos muestre el listado de 100 comics sin que aparezcan los de imagenes dañadas

--- PaginasAppJS/Comics.js
+++ PaginasAppJS/Comics.js
@@ -1,27 +1,30 @@
 const marvel = {
     render: () => {
 
-        const urlAPI = 'https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=80f1cb6951bebf766987c60e778610ca&hash=75c47e64e26d6b36f547e4c8304a29ad';
+        const urlAPI = 'https://gateway.marvel.com:443/v1/public/comics?&limit=100&ts=1&apikey=80f1cb6951bebf766987c60e778610ca&hash=75c47e64e26d6b36f547e4c8304a29ad';
         const container = document.querySelector('#marvel-row');
         let contentHTML = '';
 
         fetch(urlAPI)
-            .then(respuesta => respuesta.json())
-            .then((json) => {
-                for (const comics of json.data.results) {
+        .then(respuesta => respuesta.json())
+        .then((json) => {
+            for (const comics of json.data.results) {
+                // Para nos mostrar los que no tienen imagenes disponibles
+                if (comics.thumbnail.path !== 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available') {
                     let urlComics = comics.urls[0].url;
                     contentHTML += `
-                        <div class="col-md-4">
-                        <a href="${urlComics}" target="_blank">
-                            <img src="${comics.thumbnail.path}.${comics.thumbnail.extension}" alt="${comics.title}" class="img-thumbnail">
-                        </a>
-                        <h3 class="title">${comics.title}</h3>
-                    </div>`;
-
+                        <div class="col-lg-4 col-sm-6 col-xs-12 comics-container">
+                            <a href="${urlComics}" target="_blank" class="comics-link">
+                                <img src="${comics.thumbnail.path}.${comics.thumbnail.extension}" alt="${comics.title}" class="comics-image">
+                            </a>
+                            <h3 class="title">${comics.title}</h3>
+                        </div>`;
                 }
-                container.innerHTML = contentHTML;
-            })
-    }
+            }
+            
+            container.innerHTML = contentHTML;
+        })
+}
 
 };
 
