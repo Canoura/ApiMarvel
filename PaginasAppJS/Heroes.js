8c517107d31b9446f88ee7583fd134cd657a1272
Canoura <sthefanyacostac@gmail.com>
2024-01-30 16:36:37 +0000

    Agrego los footer en los html faltantes para tener el mismo diseño en todas mis paginas

--- PaginasAppJS/Heroes.js
+++ PaginasAppJS/Heroes.js
@@ -1,21 +1,21 @@
 const marvel = {
     render: () => {
 
-        const urlAPI = 'https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=80f1cb6951bebf766987c60e778610ca&hash=75c47e64e26d6b36f547e4c8304a29ad';
+        const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=80f1cb6951bebf766987c60e778610ca&hash=75c47e64e26d6b36f547e4c8304a29ad';
         const container = document.querySelector('#marvel-row');
         let contentHTML = '';
 
         fetch(urlAPI)
             .then(respuesta => respuesta.json())
             .then((json) => {
-                for (const comics of json.data.results) {
-                    let urlComics = comics.urls[0].url;
+                for (const heroe of json.data.results) {
+                    let urlHeroes = heroe.urls[0].url;
                     contentHTML += `
                         <div class="col-md-4">
-                        <a href="${urlComics}" target="_blank">
-                            <img src="${comics.thumbnail.path}.${comics.thumbnail.extension}" alt="${comics.title}" class="img-thumbnail">
+                        <a href="${urlHeroes}" target="_blank">
+                            <img src="${heroe.thumbnail.path}.${heroe.thumbnail.extension}" alt="${heroe.name}" class="img-thumbnail">
                         </a>
-                        <h3 class="title">${comics.title}</h3>
+                        <h3 class="title">${heroe.name}</h3>
                     </div>`;
 
                 }
