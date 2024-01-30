562d3277bcc14482a380a23d1ca8372caa3a7618
Canoura <sthefanyacostac@gmail.com>
2024-01-30 19:31:15 +0000

    Arreglamos el codigo para que nos muestre el listado de 100 heroes sin que aparezcan los de imagenes dañadas

--- PaginasAppJS/Heroes.js
+++ PaginasAppJS/Heroes.js
@@ -1,7 +1,8 @@
 const marvel = {
+
     render: () => {
 
-        const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=80f1cb6951bebf766987c60e778610ca&hash=75c47e64e26d6b36f547e4c8304a29ad';
+        const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=1&apikey=80f1cb6951bebf766987c60e778610ca&hash=75c47e64e26d6b36f547e4c8304a29ad';
         const container = document.querySelector('#marvel-row');
         let contentHTML = '';
 
@@ -9,7 +10,7 @@ const marvel = {
             .then(respuesta => respuesta.json())
             .then((json) => {
                 for (const heroe of json.data.results) {
-                    // Para nos mostrar los que no tienen imagenes disponibles 
+                    // Para nos mostrar los que no tienen imagenes disponibles
                     if (heroe.thumbnail.path !== 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available') {
                         let urlHeroes = heroe.urls[0].url;
                         contentHTML += `
